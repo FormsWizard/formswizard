6883a41d02ab080eba6f144eff23edfe173fcc7b
Johannes Lötzsch <git@johannesloetzsch.de>
2023-08-26 01:37:37 +0200

    removed packages required for decrypting submissions at processing

--- frontend/packages/project-state/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from "./features/state/store";
-export * from "./features/state/hooks";
-export * from "./features/slices/schemaSlice";
-export * from "./features/slices/keysSlice";
-export * from "./features/slices/cryptedDataSlice";
