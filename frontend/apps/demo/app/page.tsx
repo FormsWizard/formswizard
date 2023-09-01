6ad3772144cdc27ae679d18366f023ab1c95eb3b
Johannes Lötzsch <git@johannesloetzsch.de>
2023-09-01 07:04:58 +0200

    landing page: mock

--- frontend/apps/demo/app/page.tsx
+++ frontend/apps/demo/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <div>Landing Page</div>
+  return <>
+    <p>TODO: here we should add a welcome page and a brief introduction</p>
+
+    <a href="./new">Create a new form</a>
+
+    <p>TODO: in future we list the history of previousliy created forms to reopen it</p>
+  </>
 }
