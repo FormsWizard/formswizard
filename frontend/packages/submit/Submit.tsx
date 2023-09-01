25abd76c1630b17a3838fc1a5e5af0fac97e2181
winzlieb <winzlieb@c3d2.de>
2023-09-01 06:25:36 +0000

    beautify submission form

--- frontend/packages/submit/Submit.tsx
+++ frontend/packages/submit/Submit.tsx
@@ -1,61 +1,104 @@
 "use client";
 
-import { useCallback, useState } from 'react';
-import { Provider } from "react-redux";
-import { store, useAppSelector, selectJsonSchema, selectUiSchema, selectPubKeys, useAppDispatch, setCryptedData, selectCryptedData } from 'project-state';
-import { DemoYjsProvider } from 'project-state-demo-yjs';
-import { JsonForms } from '@jsonforms/react';
+import {useCallback, useState} from 'react';
+import {Provider} from "react-redux";
+import {
+  store,
+  useAppSelector,
+  selectJsonSchema,
+  selectUiSchema,
+  selectPubKeys,
+  useAppDispatch,
+  setCryptedData,
+  selectCryptedData
+} from 'project-state';
+import {DemoYjsProvider} from 'project-state-demo-yjs';
+import {JsonForms} from '@jsonforms/react';
 import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
-import { basicRenderer } from '@formswizard/designer-basic-renderer';
-import { PGPProvider, encrypt, useKeyContext } from 'pgp-provider';
-import Button from '@mui/material/Button'
-import { NoSsr } from '@mui/material';
+import {basicRenderer} from '@formswizard/designer-basic-renderer';
+import {PGPProvider, encrypt, useKeyContext} from 'pgp-provider';
+import {Button, Container, Alert, NoSsr, Grid, CircularProgress, Backdrop, Typography} from '@mui/material';
 
 function Form() {
   const [data, setData] = useState({});
+  const [published, setPublished] = useState(false);
 
-  const onChange = useCallback( ({errors, data}: {errors: any[], data: any}) => {
-    if(errors.length === 0) {
+  const onChange = useCallback(({errors, data}: { errors: any[], data: any }) => {
+    if (errors.length === 0) {
       setData(data)
-    };
+    }
+    ;
   }, []);
 
   const pubKeys = useAppSelector(selectPubKeys);
-  const { keyId, armoredPublicKey, publicKey } = useKeyContext();
+  const {keyId, armoredPublicKey, publicKey} = useKeyContext();
   const dispatch = useAppDispatch();
 
-  const onSubmit = useCallback( async () => {
+  const onSubmit = useCallback(async () => {
     const encrypted = await encrypt(JSON.stringify(data), pubKeys, publicKey);  // TODO add signature
-    if(encrypted && keyId && armoredPublicKey) {
-      const cryptedData = { data: encrypted,
-                            uuid: crypto.randomUUID(),
-                            keyId, armoredPublicKey };
+    if (encrypted && keyId && armoredPublicKey) {
+      const cryptedData = {
+        data: encrypted,
+        uuid: crypto.randomUUID(),
+        keyId, armoredPublicKey
+      };
       console.info({cryptedData});
       dispatch(setCryptedData(cryptedData));
+      setPublished(true);
     }
-  }, [data])
-  const cryptedDataPublished = useAppSelector(selectCryptedData);
-  console.log({cryptedDataPublished});
+  }, [data, setPublished])
+  //const cryptedDataPublished = useAppSelector(selectCryptedData);
 
   const jsonSchema = useAppSelector(selectJsonSchema);
   const uiSchema = useAppSelector(selectUiSchema);
 
-  return !jsonSchema ? <p>'Loading jsonSchema…'</p> : <>
-    <JsonForms
-      renderers={[...materialRenderers, ...basicRenderer]}
-      cells={materialCells}
-      schema={jsonSchema}
-      uischema={uiSchema}
-      data={data}
-      onChange={onChange}
-    />
-    <Button variant="contained" onClick={onSubmit}>
-      Submit
-    </Button>
-  </>
+  return (
+      <>
+        <Backdrop
+            sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
+            open={!jsonSchema}
+        >
+          <CircularProgress color="inherit"/>
+          Loading the schema...
+        </Backdrop>
+        <Container>
+          {jsonSchema ?
+              <Grid container spacing={2} direction={'column'}>
+                <Grid item>
+                  <Typography variant="h3" component="h3">
+                    {'Submission Form'}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <JsonForms
+                      renderers={[...materialRenderers, ...basicRenderer]}
+                      cells={materialCells}
+                      schema={jsonSchema}
+                      uischema={uiSchema}
+                      data={data}
+                      onChange={onChange}
+                      readonly={published}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={onSubmit} disabled={published}>
+                    {published ? '✅' : ''} Submit
+                  </Button>
+                </Grid>
+                {published &&
+                    <Grid item>
+                      <Alert severity={'success'}>
+                        Your data has been published.
+                      </Alert>
+                    </Grid>
+                }
+              </Grid>
+          : null}
+        </Container>
+      </>)
 }
 
 export function Submit() {
